20eae0676cab2a6caaad8678f72c53caff67c7f3
zeen <157370421+kzeen@users.noreply.github.com>
2025-03-20 16:21:22 +0200

    Implement constructor getCount of adapter

--- app/src/main/java/com/example/mobileweek8/MyAdapter.java
+++ app/src/main/java/com/example/mobileweek8/MyAdapter.java
@@ -1,13 +1,25 @@
 package com.example.mobileweek8;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.ArrayList;
+
 public class MyAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<String> studentsName;
+
+    public MyAdapter(Context context, ArrayList<String> studentsName) {
+        this.context = context;
+        this.studentsName = studentsName;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return studentsName.size();
     }
 
     @Override
@@ -22,6 +34,12 @@ public class MyAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        // Create a new view for item position "position" (param) by inflating from a layout resource
+        View generatedView = LayoutInflater.from(context).inflate(R.layout.list_item , parent, false);
+        // Get the datasource index position
+        // Fill this view with the data
+        // Return the finalized view
+
+        return generatedView;
     }
 }
